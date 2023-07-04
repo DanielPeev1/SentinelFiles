2d54d08e5ff9c1b29af371592951abbfbb15112a
Daniel Peev <39627032+DanielPeev1@users.noreply.github.com>
2023-07-04 17:18:34 +0300

    Actually fixed dimension error

--- FirstConvolutionalNeuralNetwork.py
+++ FirstConvolutionalNeuralNetwork.py
@@ -38,6 +38,14 @@ cutoff_point = int (labels.shape [0] * 0.8)
 input_train =  input [:cutoff_point]
 labels_train = labels [:cutoff_point]
 
+#Reshaping the input so as to fit the neural network dimensions
+input_train_reshaped = []
+
+for ip in input_train:
+    input_train_reshaped.append(ip.reshape (input_train [0].shape[1], input_train [0].shape[2], input_train [0].shape[0]))
+
+input_train_reshaped = np.array(input_train_reshaped)
+
 #These four lines reshape the labels array representing the lables
 #of the traininng set so that each label is a one dimensional
 #numpy array to fit with the shape of the final dense layer of the network
@@ -45,12 +53,21 @@ labels_train = labels [:cutoff_point]
 labels_train_reshaped = []
 
 for label in labels_train:
-    labels_train_reshaped.append(label.reshape(labels_train [0].shape [0], labels_train [0].shape [1] * labels_train [0].shape[2]))
+    labels_train_reshaped.append(label.reshape(labels_train [0].shape [1] * labels_train [0].shape[2], labels_train [0].shape [0]))
 labels_train_reshaped = np.array (labels_train_reshaped)
 
 #The validation sets
 input_validation =  input [cutoff_point:]
 labels_validation = labels [cutoff_point:]
+
+#Reshaping the input so as to fit the neural network dimensions
+input_validation_reshaped = []
+
+for ip in input_validation:
+    input_validation_reshaped.append( ip.reshape (input_train [0].shape[1], input_train [0].shape[2], input_train [0].shape[0]))
+
+input_validation_reshaped = np.array(input_validation_reshaped)
+
 
 #These four lines reshape the labels array representing the lables
 #of the validation set so that each label is a one dimensional
@@ -58,10 +75,10 @@ labels_validation = labels [cutoff_point:]
 labels_validation_reshaped = []
 
 for label in labels_validation:
-    labels_validation_reshaped.append(label.reshape(labels_train [0].shape [1] * labels_train [0].shape[2], labels_train [0].shape [0] ))
+    labels_validation_reshaped.append(label.reshape(labels_train [0].shape [1] * labels_train [0].shape[2], labels_train [0].shape [0]))
 
 labels_validation_reshaped = np.array (labels_validation_reshaped)
-
+print (labels_validation_reshaped.shape)
 
 #The architecture of the neural network
 #This first model is without pooling layers, which
@@ -69,7 +86,7 @@ labels_validation_reshaped = np.array (labels_validation_reshaped)
 #have rather small dimensions
 print (input_train.shape)
 model = Sequential ([
-    Conv2D(filters = 8, kernel_size = (3, 3), activation = 'sigmoid', padding = 'same', input_shape = (2, 120, 80)),
+    Conv2D(filters = 8, kernel_size = (3, 3), activation = 'sigmoid', padding = 'same', input_shape = (120, 80, 2)),
     Conv2D(filters = 16, kernel_size = (3, 3), activation = 'sigmoid', padding = 'same'),
     Conv2D(filters = 8, kernel_size = (3, 3), activation = 'sigmoid', padding = 'same'),
     Flatten(),
@@ -82,4 +99,4 @@ print (model.summary())
 model.compile (optimizer = Adam(learning_rate = 0.001), loss='mean_squared_error', metrics = [tf.keras.metrics.RootMeanSquaredError()])
 
 #Fitting the model on the data and outputing the progress on each epoch
-model.fit (input_train, labels_train_reshaped, validation_data = (input_validation, labels_validation_reshaped), epochs = 10, verbose = 2)
+model.fit (input_train_reshaped, labels_train_reshaped, validation_data = (input_validation_reshaped, labels_validation_reshaped), epochs = 10, verbose = 2)
