4d30be1b1b4790f6d7f44870119b0193aba7a358
Daniel Peev <39627032+DanielPeev1@users.noreply.github.com>
2023-07-24 13:25:42 +0300

    Added a decibel transformation to input to improve accuracy

--- OnlySARNetwork.py
+++ OnlySARNetwork.py
@@ -7,9 +7,24 @@ from keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D,
 from sklearn.model_selection import train_test_split
 from keras import Model
 from matplotlib import pyplot as plt
+from scipy.ndimage.filters import uniform_filter
+from scipy.ndimage.measurements import variance
+
+#Definition of a classical lee speckle filter
+#not used in this particular network, but meant
+#to potentially be used to transform the input SAR images
+def lee_filter(img, size):
+    img_mean = uniform_filter(img, (size, size, 1))
+    img_sqr_mean = uniform_filter(img**2, (size, size, 1))
+    img_variance = img_sqr_mean - img_mean**2
+
+    overall_variance = variance(img)
+
+    img_weights = img_variance / (img_variance + overall_variance)
+    img_output = img_mean + img_weights * (img - img_mean)
+    return img_output
 
 dataset = np.load("D:/dataset-resized.npy", allow_pickle=True)
-print(np.max(dataset [0]['sarImage'][:, :, 2]))
 
 MSE_test_values = []
 RMSE_test_values = []
@@ -17,12 +32,12 @@ dataset2 = []
 
 #Filtering out the mostly cloudy/nighttime images
 for i in dataset:
-    if np.sum(abs(i["y"])) > 1500:
+    if np.sum(abs(i["y"])) > 1200:
         dataset2.append(i)
 
 dataset = dataset2
 #K-fold cross validation
-for i in range(5):
+for i in range(1):
     dataset = shuffle(dataset)
     #Taking only the two SAR channels
     sar = [entry["sarImage"][:, :, 0:2] for entry in dataset]
@@ -32,8 +47,8 @@ for i in range(5):
 
     maxWidth = max(width)
     maxHeight = max(height)
-
-    sar = np.array([np.pad(s, [(0, maxHeight - s.shape[0]), (0, maxWidth - s.shape[1]), (0, 0)]) for s in sar])
+    #Adding a decibel-like transformation to input to improve accuracy
+    sar = np.array([np.log10(np.pad(s+1, [(0, maxHeight - s.shape[0]), (0, maxWidth - s.shape[1]), (0, 0)])) for s in sar])
     ndvi = np.array([entry["y"] for entry in dataset])
     lastNDVI = np.array([entry["sarImage"][:, :, 2] for entry in dataset])
     ids = np.array([entry["id"] for entry in dataset])
@@ -60,16 +75,19 @@ for i in range(5):
     #img = BatchNormalization()(inputSar)
     img = inputSar
     #img = MaxPool2D(pool_size=(2, 2), strides=2)(img)
-    img = Conv2D(filters=128, kernel_size=(3, 3), activation='sigmoid', padding='same')(img)
-    img = MaxPool2D(pool_size=(2, 2), strides=2)(img)
+    img = Conv2D(filters=512, kernel_size=(5, 5), activation='relu', padding='same')(img)
+    img = AveragePooling2D(pool_size=(2, 2), strides=2)(img)
     #img = Dropout(0.2)(img)
-    img = Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(img)
+    #img = Conv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same')(img)
     #img = MaxPool2D(pool_size=(2, 2), strides=2)(img)
-    img = Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same')(img)
+    '''img = Dropout(0.2)(img)
+    img = Conv2D(filters=256, kernel_size=(5, 5), activation='relu', padding='same')(img)
+    img = MaxPool2D(pool_size=(2, 2), strides=2)(img)
+    img = Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(img)
     img = Dropout(0.2)(img)
     #img = AveragePooling2D(pool_size=(2, 2), strides=2)(img)
     #img = Conv2D(filters=128, kernel_size=(3, 3), activation='sigmoid', padding='same')(img)
-    img = Conv2D(filters=256, kernel_size=(3, 3), activation='sigmoid', padding='same')(img)
+    img = Conv2D(filters=512, kernel_size=(5, 5), activation='relu', padding='same')(img)
 
     #img = Conv2D(filters=1024, kernel_size=(3, 3), activation='sigmoid', padding='same')(img)
     #img = Conv2D(filters=1024, kernel_size=(3, 3), activation='sigmoid', padding='same')(img)
@@ -78,9 +96,9 @@ for i in range(5):
     #img = Dropout(0.2)(img)
     #img = Conv2D(filters=2048, kernel_size=(3, 3), activation='sigmoid', padding='same')(img)
 
-    img = AveragePooling2D(pool_size=(2, 2), strides=2)(img)
+    img = MaxPool2D(pool_size=(2, 2), strides=2)(img)
     #img = Dropout(0.2)(img)
-    #img = Dropout(0.2)(img)
+    #img = Dropout(0.2)(img)'''
     img = Flatten()(img)
     #img = Model(inputs=inputSar, outputs=img)
     model = img
@@ -96,7 +114,7 @@ for i in range(5):
     history = model.fit(
         x = trainX,
         y=trainY,
-        epochs=20,
+        epochs=35,
         verbose='auto',
         validation_split=0.2)
 
@@ -128,12 +146,12 @@ for i in range(5):
         plt.matshow(np.resize(ndviReshaped[i:(i + 1)], (114, 71)), vmin=0, vmax=1)
         plt.savefig(ids[i - 1] + "Reality.png")
         plt.close()
-        plt.matshow(sarLayer1, vmin=np.min(sarLayer1), vmax=np.max(sarLayer1))
-        plt.savefig(ids[i - 1] + "SAR1eality.png")
-        plt.close()
-        plt.matshow(sarLayer2, vmin=np.min(sarLayer2), vmax=np.max(sarLayer2))
-        plt.savefig(ids[i - 1] + "SAR2eality.png")
-        plt.close()
+        #plt.matshow(sarLayer1, vmin=np.min(sarLayer1), vmax=np.max(sarLayer1))
+        #plt.savefig(ids[i - 1] + "SAR1eality.png")
+        #plt.close()
+        #plt.matshow(sarLayer2, vmin=np.min(sarLayer2), vmax=np.max(sarLayer2))
+        #plt.savefig(ids[i - 1] + "SAR2eality.png")
+        #plt.close()
 
         print (np.sum (abs(oldNDVI - np.resize(ndviReshaped[i:(i + 1)], (114, 71)))))
         oldNDVI = np.resize(ndviReshaped[i:(i + 1)], (114, 71))
